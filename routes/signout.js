6754fdda1a2706ff382e228a30628836d26f9b29
zigang93 <tanzigang93@gmail.com>
2018-08-06 15:17:34 +0800

    update ignore public img folder and add signout working function

--- routes/signout.js
+++ routes/signout.js
@@ -5,7 +5,11 @@ const checkLogin = require('../middlewares/check').checkLogin;
 
 // GET /signout , Sign Out
 router.get('/', checkLogin, function (req, res, next) {
-  res.send('Sign Out');
+  // Clear session user info
+  req.session.user = null;
+  req.flash('success', 'Success Sign Out');
+  // After Sign Out, redirect to home page.
+  res.redirect('/posts');
 });
 
 module.exports = router;
